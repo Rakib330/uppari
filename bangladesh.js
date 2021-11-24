cc5392453967c1cae65d1fc125a7f0cb6634642c
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2021-11-24 21:38:18 +0600

    Add files via upload

---
+++ bangladesh.js
@@ -0,0 +1,157 @@
+var bangladesh =   [
+					//rangpur
+					[[ThakurgaonSadar,Baliadangi,Ranisankail,thPirganj,Haripur],
+	               [RangpurSadar,Kaunia,Gangachara,Badarganj,Taraganj,raPirganj,Pirgachha,Mithapukur],
+	               [PanchagarhSadar,Boda,Atwari,Tetulia,Debiganj],
+                   [NilphamariSadar,Dimla,Saidpur,Kishoreganj,Jaldhaka,Domar],
+	               [LalmonirhatSadar,Patgram,laKaliganj,Hatibandha,Aditmari],
+	               [KurigramSadar,Bhurungamari,Ulipur,Rowmari,Rajarhat,kuPhulbari,Nageshwari,Chilmari,CharRajibpur],
+	               [GaibandhaSadar,Sundarganj,Saghata,Sadullapur,Palashbari,Gobindaganj,Fulchhari],
+	               [DinajpurSadar,Chirirbandar,Bochaganj,Birganj,Birampur,Biral,diPhulbari,Parbatipur,diNawabganj,Khansama,
+                    Kaharole,Hakimpur,Ghoraghat]],
+					//rajshahi
+					[[SirajganjSadar,Chauhali,Belkuchi,Ullapara,Tarash,Shahjadpur,Raiganj,Kazipur,Kamarkhanda],
+	               [RajshahiCity,Charghat,Bagmara,Bagha,Tanore,Puthia,Mohanpur,Godagari,raDurgapur],
+	               [PabnaSadar,Sujanagar,Santhia,Faridpur,Chatmohar,Bhangura,Bera,Atgharia,Ishwardi],
+                   [NatoreSadar,Baraigram,Bagatipara,Singra,Lalpur,Gurudaspur],
+	               [NaogaonSadar,Badalgachhi,Atrai,Sapahar,Raninagar,Porsha,Patnitala,Niamatpur,Mohadevpur,Manda,Dhamoirhat],
+	               [JoypurhatSadar,Akkelpur,Panchbibi,Khetlal,Kalai],
+	               [ChapaiNawabganjSadar,chShibganj,Nachole,Gomastapur,Bholahat],
+	               [BograSadar,Sonatola,boShibganj,Sherpur,Shajahanpur,Sariakandi,Nandigram,Kahaloo,Gabtali,Dupchanchia,Dhunat,Adamdighi]],
+				   //mymensingh
+				   [[MymensinghSadar,Gouripur,Gafargaon,Fulbaria,Dhobaura,Bhaluka,Trishal,Phulpur,Nandail,Muktagachha,Ishwarganj,Haluaghat],
+	               [JamalpurSadar,Islampur,Dewanganj,Bakshiganj,Sarishabari,Melandaha,Madarganj],
+	               [NetrokonaSadar,Atpara,Purbadhala,Mohanganj,Madan,Khaliajuri,Kendua,Kalmakanda,neDurgapur,Barhatta],
+	               [SherpurSadar,Jhenaigati,Sreebardi,Nalitabari,Nakla]],
+				   //sylhet
+				   [[SylhetSadar,Beanibazar,Balaganj,Zakiganj,Kanaighat,Jaintiapur,Gowainghat,Golapganj,Fenchuganj,DakshinSurma,sCompaniganj,Bishwanath],
+                   [HabiganjSadar,Nabiganj,Madhabpur,Lakhai,Chunarughat,Baniachong,Bahubal,Ajmiriganj],
+                   [Bishwamvarpur,Chhatak,DakshinSunamganj,Derai,Dharmapasha,Dowarabazar,Jagannathpur,Jamalganj,Sullah,Tahirpur,SunamganjSadar],
+	               [MoulvibazarSadar,Juri,Barlekha,Sreemangal,Rajnagar,Kulaura,Kamalganj]],
+				   //dhaka
+				   [[DhakaCity,Savar,dhNawabganj,Keraniganj,Dohar,Dhamrai],
+	               [GazipurSadar,Sreepur,Kapasia,gaKaliganj,Kaliakair],
+	               [KishoreganjSadar,Bajidpur,Austagram,Tarail,Pakundia,Nikli,Mithamain,Kuliarchar,Katiadi,Karimganj,Itna,Hossainpur,Bhairab],
+	               [ManikganjSadar,maDaulatpur,Singair,Shibalaya,Saturia,Harirampur,Ghior],
+	               [MunshiganjSadar,Gazaria,Tongibari,Sreenagar,Sirajdikhan,Louhajang],
+	               [NarayanganjSadar,Bandar,Araihazar,Sonargaon,Rupganj],
+	               [NarsingdiSadar,Monohardi,Belabo,Shibpur,Raipura,Palash],
+	               [TangailSadar,Delduar,Basail,Sakhipur,Bhuapur,Nagarpur,Mirzapur,Madhupur,Kalihati,Gopalpur,Ghatail,Dhanbari],
+                   [FaridpurSadar,Alfadanga,Saltha,Sadarpur,Nagarkanda,Madhukhali,Charbhadrasan,Boalmari,Bhanga],
+	               [GopalganjSadar,Kashiani,Tungipara,Musksudpur,Kotalipara],
+	               [MadaripurSadar,Kalkini,Shibchar,Rajoir],
+	               [RajbariSadar,Kalukhali,Goalandaghat,Baliakandi,Pangsha],
+	               [ShariatpurSadar,Gosairhat,Damudya,Bhedarganj,Zajira,Naria]],
+				   //khulna
+				   [[KhulnaCity,Dacope,Batiaghata,Terokhada,Rupsha,Phultala,Paikgachha,Koyra,Dumuria,Dighalia],
+	               [BagerhatSadar,Sarankhola,Rampal,Morrelganj,Mongla,Mollahat,baKachua,Fakirhat,Chitalmari],
+	               [SatkhiraSadar,Debhata,Assasuni,Tala,Shyamnagar,saKaliganj,Kalaroa],
+	               [JessoreSadar,Sharsha,Manirampur,Keshabpur,Jhikargachha,Chaugachha,Bagherpara,Abhaynagar],
+	               [ChuadangaSadar,Jibannagar,Damurhuda,Alamdanga],
+	               [JhenaidahSadar,Harinakunda,Shailkupa,Maheshpur,Kotchandpur,jhKaliganj],
+	               [KushtiaSadar,kuDaulatpur,Bheramara,Mirpur,Kumarkhali,Khoksha],
+	               [MaguraSadar,Mohammadpur,Sripur,Shalikha],
+	               [MeherpurSadar,Gangni,Mujibnagar],
+	               [NarailSadar,noLohagara,Kalia]],
+				   //barisal
+				   [[BarisalSadar,Gaurnadi,Hizla,Mehendiganj,Muladi,Wazirpur,Agailjhara,Babuganj,Bakerganj,Banaripara],
+	               [PirojpurSadar,piKawkhali,Bhandaria,Zianagar,Nesarabad,Nazirpur,Mathbaria],
+	               [PatuakhaliSadar,Bauphal,Mirzaganj,Kalapara,Galachipa,Dumki,Dashmina],
+	               [JhalokatiSadar,Rajapur,Nalchity,Kanthalia],
+	               [BholaSadar,Tazumuddin,Manpura,Lalmohan,Daulatkhan,CharFasson,Borhanuddin],
+	               [BargunaSadar,Betagi,Patharghata,Amtali,Bamna]],
+				   //chittagong
+				   [[ChittagongCity,Chandanaish,Boalkhali,Banshkhali,Anwara,Hathazari,Fatikchhari,Sitakund,Satkania,Sandwip,Raozan,Rangunia,Patiya,Mirsharai,Lohagara],
+                   [KhagrachhariSadar,Mahalchhari,Lakshmichhari,Dighinala,Ramgarh,Panchhari,Matiranga,Manikchhari],
+                   [CoxsBazarSadar,Chakaria,Ukhiya,Teknaf,Ramu,Pekua,Moheskhali,Kutubdia],
+                   [BandarbanSadar,Alikadam,Thanchi,Ruma,Rowangchhari,Naikhongchhari,Lama],
+                   [RangamatiSadar,Rajasthali,Naniarchar,Langadu,raKawkhali,Kaptai,Juraichhari,Belaichhari,Barkal,Baghaichhari],
+                   [NoakhaliSadar,Subarnachar,Sonaimuri,Senbag,Kabirhat,Hatia,Companiganj,Chatkhil,Begumganj],
+	               [LakshmipurSadar,Kamalnagar,Ramgati,Ramganj,Raipur],
+	               [FeniSadar,Fulgazi,Daganbhuiyan,Chhagalnaiya,Sonagazi,Parshuram],[ComillaAdarshaSadar,Chauddagram,Chandina,Burichang,Brahmanpara,Barura,Titas,Nangalkot,Muradnagar,Monoharganj,Meghna,Laksam,Homna,Debidwar,Daudkandi,ComillaSadarDakshin],
+	               [ChandpurSadar,Faridganj,Shahrasti,MatlabUttar,MatlabDakshin,Kachua,Hajiganj,Haimchar],
+	               [BrahmanbariaSadar,Akhaura,Sarail,Nasirnagar,Nabinagar,Kasba,Bijoynagar,Bancharampur,Ashuganj]]
+					
+					];
+
+var upazila = document.getElementByClassName("upazila");
+//function bangladesh(){
+//			for(var x=0; x<bangladesh.length;x++){
+//                for(var y=0; y<bangladesh[x].length;y++){
+//					for(var z=0; z<bangladesh[x][y].length;z++){
+//                        bangladesh[x][y][z].classList.toggle("add");
+//					}
+//                }
+//			}
+//		}
+
+
+//active upazila
+function aupazila(){
+    for(var x=0; x<bangladesh.length;x++){
+        for(var y=0; y<bangladesh[x].length;y++){
+            for(var z=0; z<bangladesh[x][y].length;z++){
+                bangladesh[x][y][z].onclick= function activeupazila(){
+                    if(document.getElementByClassName("upazila") == clicked){
+                        upazila.classList.remove("actives");
+                        (this).classList.add("actives");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+//add upazila
+function addupazila(){
+    for(var x=0; x<bangladesh.length;x++){
+        for(var y=0; y<bangladesh[x].length;y++){
+            for(var z=0; z<bangladesh[x][y].length;z++){
+                bangladesh[x][y][z].onclick= function addupazila(){
+                    (this).classList.toggle("actives");
+                }
+            }
+        }
+    }
+}
+//add upazilar
+function addupazilar(){
+    for(var x=0; x<bangladesh.length;x++){
+        for(var y=0; y<bangladesh[x].length;y++){
+            for(var z=0; z<bangladesh[x][y].length;z++){
+                bangladesh[x][y][z].onclick= function addupazilar(){
+                    (this).classList.toggle("activesr");
+                }
+            }
+        }
+    }
+}
+//add upazilab
+function addupazilab(){
+    for(var x=0; x<bangladesh.length;x++){
+        for(var y=0; y<bangladesh[x].length;y++){
+            for(var z=0; z<bangladesh[x][y].length;z++){
+                bangladesh[x][y][z].onclick= function addupazilab(){
+                    (this).classList.toggle("activesb");
+                }
+            }
+        }
+    }
+}
+//add upazilay
+function addupazilay(){
+    for(var x=0; x<bangladesh.length;x++){
+        for(var y=0; y<bangladesh[x].length;y++){
+            for(var z=0; z<bangladesh[x][y].length;z++){
+                bangladesh[x][y][z].onclick= function addupazilay(){
+                    (this).classList.toggle("activesy");
+                }
+            }
+        }
+    }
+}
+
+    
