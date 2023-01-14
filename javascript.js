9e8a6a79620aade367bfba166625b4d83db06f8a
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-01-14 13:15:54 +0600

    bv

--- javascript.js
+++ javascript.js
@@ -16,17 +16,17 @@
 
 function fetchData() {
 fetch('./data.json')
-    .then((res) => JSON.stringify(res))
+    //.then((res) => JSON.stringify(res))
     .then((data) => {
         console.log(data)
-        document.getElementById('allmap').innerHTML += `<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[0].title}">
-                    <g class="upazila additonalupazila" id="${mydata[0].name}">
-${mydata[0].path}
+        document.getElementById('allmap').innerHTML += `<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${data[0].title}">
+                    <g class="upazila additonalupazila" id="${data[0].name}">
+${data[0].path}
 </g>
 </a>
-<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[1].title}">
-                    <g class="upazila additonalupazila" id="${mydata[1].name}">
-${mydata[1].path}
+<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${data[1].title}">
+                    <g class="upazila additonalupazila" id="${data[1].name}">
+${data[1].path}
 </g>
 </a>`;
         })
