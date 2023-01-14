940d0469a9ac8c3323f87f8a64e7ee49e2172cd7
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-01-14 13:01:27 +0600

    cs

--- javascript.js
+++ javascript.js
@@ -1,7 +1,25 @@
 
-import data from './data.json' assert {type: 'json'};
-var mydata = JSON.parse(data);
-document.getElementById('allmap').innerHTML += `<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[0].title}">
+//import data from './data.json' assert {type: 'json'};
+//var mydata = JSON.parse(data);
+//document.getElementById('allmap').innerHTML += `<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[0].title}">
+//                    <g class="upazila additonalupazila" id="${mydata[0].name}">
+//${mydata[0].path}
+//</g>
+//</a>
+//<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[1].title}">
+//                    <g class="upazila additonalupazila" id="${mydata[1].name}">
+//${mydata[1].path}
+//</g>
+//</a>`;
+
+
+
+function fetchData() {
+fetch('./data.json')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        document.getElementById('allmap').innerHTML += `<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[0].title}">
                     <g class="upazila additonalupazila" id="${mydata[0].name}">
 ${mydata[0].path}
 </g>
@@ -11,5 +29,12 @@ ${mydata[0].path}
 ${mydata[1].path}
 </g>
 </a>`;
+        })
+            .catch((error) => {
+                console.log(`Error Fetching data : ${error}`)
+                document.getElementById('country').innerHTML = 'Error Loading Data'
+            });
 
+}
 
+fetchData()
