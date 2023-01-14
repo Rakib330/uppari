d584ed1f21fa51a9bf7129bdeba95c2798c39a96
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-01-14 12:53:03 +0600

    sd

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 
-import mydata from './data.json' assert {type: 'json'};
-//var mydata = JSON.parse(data);
+import data from './data.json' assert {type: 'json'};
+var mydata = JSON.parse(data);
 document.getElementById('allmap').innerHTML += `<a onmouseover="val(this.getAttribute('xlink:title'))" xlink:title="${mydata[0].title}">
                     <g class="upazila additonalupazila" id="${mydata[0].name}">
 ${mydata[0].path}
